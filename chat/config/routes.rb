43839d05908ab71ef9dea8fe052ae67d8682cfdc
Remi <remi.hernandez@epitech.eu>
2014-09-26 00:41:54 +0200

    add index page

--- chat/config/routes.rb
+++ chat/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :posts
 
   resources :users
+
+  root :to => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
